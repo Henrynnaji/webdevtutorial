c3dc5acb5e6e20395c420f69abf0f37ca8d47200
Henry Nnaji <henrynnajichukwu@gmail.com>
2019-08-04 18:31:19 +0100

    My first class work

---
+++ Exercises/firstexercise.js
@@ -0,0 +1,28 @@
+console.log("My exercise is working perfectly");
+const num1 = 28;
+const num2 = 32;
+const num3 = 3;
+const num4 = 23;
+const num5 = 72;
+const num6 = 12;
+const num7 = 14;
+const num8 = 11;
+const num9 = 20;
+const num10 = 44;
+
+let sum = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+
+console.log("sum: "+sum);
+
+let average =sum/10;
+
+console.log("average: "+average);
+
+//Exercise 2: Area of a circle
+let lb="\n";
+console.log(lb);
+console.log("Area of a circle");
+let pie = 3.142;
+let r = 4;
+let Area = pie*r*r;
+console.log("Area of a circle: "+Area);
---
+++ Tutorials/firsttutorial.js
@@ -0,0 +1,16 @@
+console.log(32+25)
+let firstnumber = 28;
+const secondnumber = 2;
+const thirdnumber = 62;
+let sum = firstnumber+secondnumber+thirdnumber;
+let average = sum/3
+
+console.log(average)
+//
+
+//working with strings
+let firstword = "bread ";
+let secondword ="tea";
+let breakfast = firstword +"\n"+secondword;
+console.log(breakfast)
+console.log("sum: "+sum);
